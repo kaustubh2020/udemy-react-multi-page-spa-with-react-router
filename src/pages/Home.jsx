3bc9a14b3893c2d32a80032a3a22e04f539d85c6
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2024-05-03 23:33:24 +0530

    Navigate Programatically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  function useNavigateHandler() {
+    navigate("/about");
+  }
+
   return (
     <>
       <h1>HOME</h1>
       Go to <Link to={"/about"}>About Page</Link>.
+      <p>
+        <button onClick={useNavigateHandler}>Navigate</button>
+      </p>
     </>
   );
 };
