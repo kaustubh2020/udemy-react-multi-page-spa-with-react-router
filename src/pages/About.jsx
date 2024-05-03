efbdf60931e5156b3a1a66c3fb91db2fb9cc810f
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2024-05-03 23:55:42 +0530

    Links for Dynamic Routes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,21 @@
+import { Link } from "react-router-dom";
+
 const About = () => {
+  const ITEMS = [
+    { id: "item-1", title: "Item 1" },
+    { id: "item-2", title: "Item 2" },
+    { id: "item-3", title: "Item 3" },
+  ];
+
   return (
     <>
       <h1>About</h1>
       <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
+        {ITEMS.map((item) => (
+          <li key={item.id}>
+            <Link to={`/item/${item.id}`}>{item.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
